650e669d17392b7da425e3c8a2e8d39c28d69719
mauriciomanjarres-polaria <mauricio.manjarres@polaria.tech>
2026-05-12 09:32:36 -0500

    Update scrapeController.js

--- scrapers/scrapeController.js
+++ scrapers/scrapeController.js
@@ -108,21 +108,3 @@ const scrapeGeneral = async (req, res) => {
 
 // 2. IMPORTANTE: Exportamos la función para que index.js la vea
 module.exports = { scrapeGeneral };
-¿Cómo queda ahora tu index.js?
-Este archivo solo debe tener las rutas.
-
-JavaScript
-const express = require('express');
-const app = express();
-const { scrapeGeneral } = require('./scrapers/scrapeController');
-
-app.get('/scrape', scrapeGeneral);
-
-// Aquí podrás agregar fácilmente:
-// const { scrapeTCI } = require('./scrapers/tciController');
-// app.get('/tci', scrapeTCI);
-
-const PORT = process.env.PORT || 3000;
-app.listen(PORT, '0.0.0.0', () => {
-    console.log(`Servidor en puerto ${PORT}`);
-});
